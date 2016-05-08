189c9692379dab57f4d6c109ffe94ed87c110c7e
Bosco Ho <apposite@gmail.com>
2016-05-08 11:27:00 +1000

    improved buildwebgl with error reporting and watch mode; now uses gulp

--- buildwebgl.js
+++ buildwebgl.js
@@ -2,18 +2,23 @@
 
 "use strict";
 
-var fs = require("fs");
-var path = require("path");
-var browserify = require("browserify");
-var watchify = require("watchify");
-var babelify = require("babelify");
-var cheerio = require("cheerio");
-var prettyHtml = require("html");
+let fs = require("fs");
+let path = require("path");
+
 let nopt = require("nopt");
+
+let browserify = require("browserify");
+let babelify = require("babelify");
+
+let gulp = require('gulp');
+let rename = require("gulp-rename");
+let source = require('vinyl-source-stream');
+let transform = require('vinyl-transform');
+
+let prettyHtml = require("html");
 
 
 let doc = `
-
 buildwebgl.js - compiles a WebGLStartKit ES2015 file into a working website
 
 usage: buildwebgl.js [--watch -w] sample.js
@@ -23,16 +28,14 @@ usage: buildwebgl.js [--watch -w] sample.js
 
 Will compile downto sample.compiled.js and create sample.html that will
 display the WebGL graphics.
-
-`
+`;
 
 
 function buildHtml(html, selector, outScript) {
+    let outScriptBase = path.basename(outScript);
 
-    var outScriptBase = path.basename(outScript);
-
-    var htmlText =
-    `<html>
+    let htmlText =
+        `<html>
     <head>
     <style>
         body {
@@ -55,34 +58,42 @@ function buildHtml(html, selector, outScript) {
             { indent_size: 2 }
         )
     );
-
 }
 
 
-function checkHtml(html, selector, outScript) {
-
-    var tree = cheerio.load(fs.readFileSync(html));
-
-    var root = tree.root();
-    var outScriptBase = path.basename(outScript);
-
-    if ( tree( `script[src="${outScriptBase}"]`).length === 0 ) {
-        root.append(`<script src="${outScriptBase}"></script>`);
-    }
-
-    if ( tree( `div[id="${selector}"]`).length === 0 ) {
-        let body = tree( "body" );
-        if ( body.length === 0 ) {
-            root.append("<body>");
-            body = tree( "body");
+function buildErrorHtml(html, errorString) {
+    let htmlText =
+        `<html>
+    <head>
+    <style>
+        body {
+            margin: 10px;
+            font-family: Arial;
         }
+    </style>
+    </head>
+    <body>
+        <h2>There were errors in compiling:</h2>
+        <div style="font-family:monospace">
+             ${errorString}
+        </div>
+    </body>
+    </html>`;
 
-        body.append(`<div id="${selector}" style="width:100%; height:100%;">`);
-    }
-
-    fs.writeFile(html, tree.html());
+    fs.writeFile(
+        html,
+        prettyHtml.prettyPrint(
+            htmlText,
+            { indent_size: 2 }
+        )
+    );
 }
 
+function swallowError (error) {
+    isError = true;
+    errorString = error.toString();
+    this.emit('end');
+}
 
 let knownOpts = { "watch": [Boolean, false] };
 let shortHands = { "w": ["--watch"] };
@@ -90,71 +101,55 @@ let parsed = nopt(knownOpts, shortHands, process.argv, 2);
 let remain = parsed.argv.remain;
 
 if ( remain.length === 0 ) {
-
     console.log(doc);
-
-}
-else {
-
-    const es6Script = remain[0];
-
-    var ext = path.extname(es6Script);
-    var base = es6Script.replace(ext, "");
-    var outScript = `${base}.compiled${ext}`;
-
-    var html = `${base}.html`;
-
-    let selector = "widget";
-
-    var isError = false;
-
-    var success = function() {
-        console.log('success', isError);
-        if (!fs.existsSync(html)) {
-            console.log( "Creating", outScript, "->", selector, "->", html);
-            buildHtml(html, selector, outScript);
-        }
-        else {
-            console.log( "Checking", outScript, "->", selector, "->", html);
-            checkHtml(html, selector, outScript);
-        }
-    }
-
-    console.log( es6Script, "->", outScript);
-
-    var plugins = [];
-    if (parsed.watch) {
-        plugins.push(watchify);
-    }
-    var bundler = browserify({
-        cache: {},
-        packageCache: {},
-        entries: [es6Script],
-        plugin: plugins,
-        debug: true
-    });
-
-    function bundle() {
-        bundler.bundle().pipe(fs.createWriteStream(outScript));
-        success();
-    }
-
-    bundler.on("update", bundle);
-    // bundler.on("error", (err) => {
-    //     isError = true;
-    //     console.log('exists', fs.existsSync(outScript));
-    //     // console.log(err.message);
-    //     this.emit("end");
-    // });
-
-    try {
-        bundler.transform(babelify, { presets: "es2015" });
-    } catch (e) {
-        console.log("error", e);
-    }
-
-    bundle();
-
+    process.exit();
 }
 
+const inputScript = remain[0];
+const ext = path.extname(inputScript);
+const base = inputScript.replace(ext, "");
+const outScript = `${base}.compiled${ext}`;
+const html = `${base}.html`;
+
+const selector = "widget";
+
+let isError = false;
+let errorString = "";
+
+
+gulp.task('build', () => {
+    console.log( `Bundling and transpiling ${inputScript} to ${outScript}` );
+    return (
+        browserify({entries: inputScript, debug: true})
+            .transform(babelify, { presets: "es2015" })
+            .bundle()
+            .on('error', swallowError)
+            .pipe(source(outScript))
+            .pipe(gulp.dest('.'))
+    );
+});
+
+gulp.task('make-html', ['build'], () => {
+    if (!isError) {
+        console.log("Succesfully made", outScript);
+        console.log(
+            `Creating ${html} with elemnt #${selector} for ${outScript}`);
+        buildHtml(html, selector, outScript);
+    } else {
+        console.log(`Error in building ${outScript}`);
+        buildErrorHtml(html, errorString);
+    }
+});
+
+if (parsed.watch) {
+    gulp.task(
+        'watch',
+        ['build', 'make-html'],
+        () => {
+            gulp.watch(inputScript, ['make-html']);
+        });
+    gulp.start('watch');
+} else {
+    gulp.start(['build', 'make-html']);
+}
 
